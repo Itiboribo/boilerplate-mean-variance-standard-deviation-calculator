fe2af6b59bb615a9e2562d0080ee80a1327f5648
Kenechukwu Akujieze <101412844+Itiboribo@users.noreply.github.com>
2022-10-31 15:42:34 +0000

    Initial commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,16 @@
 import numpy as np
 
 def calculate(list):
-
-
-
+    list_matrix = np.array(list).reshape(3,3)
+    calculations = {
+      'mean': [np.mean(list_matrix, axis=0), np.mean(list_matrix, axis=1), 
+       np.mean(list_matrix)],
+      'variance': [np.var(list_matrix, axis=0), np.var(list_matrix, axis=1), 
+       np.var(list_matrix)],
+      'standard deviation': [np.std(list_matrix, axis=0), np.std(list_matrix, axis=1),                np.std(list_matrix)],
+      'max': [np.max(list_matrix, axis=0), np.max(list_matrix, axis=1), np.max(list_matrix)],
+      'min': [np.min(list_matrix, axis=0), np.min(list_matrix, axis=1), np.min(list_matrix)],
+      'sum': [np.sum(list_matrix, axis=0), np.sum(list_matrix, axis=1), np.sum(list_matrix)]
+    } 
 
     return calculations
